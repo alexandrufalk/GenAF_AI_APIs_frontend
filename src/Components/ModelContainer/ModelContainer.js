6427a5336336f166fd563ffc0ca06cc420dbd63b
Falk <alexandru.falk@yahoo.com>
2024-08-25 07:42:59 +0300

    ModelContainer updated with dinamic content

--- src/Components/ModelContainer/ModelContainer.js
+++ src/Components/ModelContainer/ModelContainer.js
@@ -2,9 +2,28 @@ import React from "react";
 import "./ModelContainer.css";
 
 const ModelContainer = () => {
+  const models = [
+    { name: "Model 1", description: "Model 1 Description" },
+    { name: "Model 2", description: "Model 2 Description" },
+    { name: "Model 3", description: "Model 3 Description" },
+    // Add more models here...
+  ];
   return (
-    <div>
-      <p>Test Container</p>
+    <div className="modelscon">
+      {models.map((model, index) => (
+        <div key={index} className="modelcon">
+          <div className="modelbody">
+            <div className="modeltitle">{model.name}</div>
+            <div className="modeldescription">{model.description}</div>
+          </div>
+        </div>
+      ))}
+      {/* <div className="modelcon">
+        <div className="modelbody">
+          <div className="modeltitle">Test Container 1</div>
+          <div className="modeldescription">Model Desription</div>
+        </div>
+      </div> */}
     </div>
   );
 };
