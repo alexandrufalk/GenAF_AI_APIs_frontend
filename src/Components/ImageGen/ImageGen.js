3937c0a5d313cbf7cf44665192b51977447a55ad
Falk <uidw6498@contiwan.com>
2024-09-11 08:34:01 +0300

    imageGen style and description added

--- src/Components/ImageGen/ImageGen.js
+++ src/Components/ImageGen/ImageGen.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import "../ObjectDetection/ObjectDetection.css";
 
-function ImageGen() {
+function ImageGen({ model }) {
   const [prompt, setPrompt] = useState("");
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const outputLines = model.output.split("\n");
+  const hasMultipleLines = outputLines.length > 1;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -12,7 +16,7 @@ function ImageGen() {
     setError(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/generate", {
+      const response = await fetch("http://localhost:5010//generate2", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -35,19 +39,55 @@ function ImageGen() {
   };
 
   return (
-    <div className="App">
-      <h1>Generate Image with Stable Diffusion</h1>
+    <div className="main-body">
+      <div className="body-header">
+        <div className="body-title">{model.name}</div>
+        <div className="body-description">{model.description}</div>
+      </div>
+      <div className="body-header">
+        <div className="body-title">Model Details</div>
+        <div className="body-description">Description</div>
+        <div className="body-subtitle">Input</div>
+        <div className="body-description">
+          <p>{model.input}</p>
+        </div>
+        <div className="body-subtitle">Output</div>
+        <div className="body-description">
+          {hasMultipleLines ? (
+            <ul>
+              {outputLines.map((line, index) => {
+                const [boldText, regularText] = line.split(":");
+
+                return (
+                  <li key={index}>
+                    <strong>{boldText}:</strong>
+                    {regularText}
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>{model.output}</p>
+          )}
+        </div>
+      </div>
+      <div className="body-header">
+        <div className="body-subtitle">Test Image Generation</div>
+      </div>
 
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter prompt"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button type="submit" disabled={loading}>
-          Generate
-        </button>
+        <div className="buttons-container">
+          <input
+            className="prompt"
+            type="text"
+            placeholder="Enter prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button className="button" type="submit" disabled={loading}>
+            Generate
+          </button>
+        </div>
       </form>
 
       {loading && <p>Generating image...</p>}
